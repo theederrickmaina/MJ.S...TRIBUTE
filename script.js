743e3eb94c153d36b21f7b0f0cc3daed65ef7ff7
theederrickmaina <146960594+theederrickmaina@users.noreply.github.com>
2023-10-27 14:56:17 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let slideIndex = 0;
-showSlides();
+const slides = document.querySelectorAll('.slide');
 
 function showSlides() {
-  let i;
-  const slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1; }    
-  slides[slideIndex - 1].style.display = "block";  
-  setTimeout(showSlides, 2000); // Change slide every 2 seconds (2000 milliseconds)
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); 
 }
 
+showSlides(); 
